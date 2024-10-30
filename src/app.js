8aa9cf47cf37dfbdebdf6ebb04796826c95156fc
tobalo1986 <116591480+tobalo1986@users.noreply.github.com>
2024-10-30 13:30:14 +0100

    prueba video

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 // Ruta básica
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello cristia!' });
+    res.status(200).json({ message: 'Hello cristian!' });
 });
 
 // Ruta de ejemplo para pruebas
